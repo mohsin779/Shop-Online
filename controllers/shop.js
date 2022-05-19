c2b175cb288cb72c24dfa9f3b9c800c48c52bf15
Mohsin <mianmohsin779@gmail.com>
2022-05-19 18:13:56 +0500

    Delete Product Functionality added

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
 const Cart=require('../models/cart');
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
@@ -29,10 +30,24 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  });
+  Cart.getCart(cart=>{
+    Product.fetchAll(products=>{
+      const cartProducts=[];
+      for(product of products){
+        const cartProductData=cart.products.find(prod=>prod.id===product.id);
+        if(cartProductData){
+          cartProducts.push({productData:product, qty:cartProductData.qty});
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products:cartProducts
+      });
+    })
+    
+  })
+  
 };
 
 exports.postCart=(req,res,next)=>{
@@ -42,6 +57,15 @@ exports.postCart=(req,res,next)=>{
   });
   res.redirect('/cart');
 }
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
+};
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
