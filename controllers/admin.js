7d37d7670cd09c4081f45732cb90cd43c2e22ef5
Mohsin <mianmohsin779@gmail.com>
2022-05-19 12:36:42 +0500

    First Push

---
+++ controllers/admin.js
@@ -0,0 +1,56 @@
+const { redirect } = require('express/lib/response');
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing:false
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(null,title, imageUrl, description, price);
+  product.save();
+  res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+
+  const editMode=req.query.edit;
+  if(!editMode){
+    return res.redirect('/');
+  }
+
+  const prodId=req.params.productId;// here we are getting this productId from url parameters
+  Product.findById(prodId,product=>{
+    if(!product){
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing:editMode,
+      product:product
+    });
+  });
+
+};
+
+exports.postEditProduct=(req,res,next)=>{
+
+}
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
+    });
+  });
+};
