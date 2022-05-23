ff5ef378f26f364b40f415d76c0d6fabe8f8b3ca
Mohsin <mianmohsin779@gmail.com>
2022-05-23 15:26:55 +0500

    converted all SQL commands into sequel commands

--- util/database.js
+++ util/database.js
@@ -1,10 +1,26 @@
-const mysql=require('mysql2') //npm install --save mysql2
+//By using  simple SQL
+// const mysql=require('mysql2') //npm install --save mysql2
 
-const pool=mysql.createPool({
+// const pool=mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     database:'node-complete',
+//     password:'9844'
+// });
+
+// module.exports =pool.promise();
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+//By using sequelize 
+//to install sequelize we also need to first install mysql2
+
+const Sequelize = require('sequelize'); //npm install --save sequelize
+
+// const Sequelize =new Sequelize('databaseName','root','Password');
+const sequelize = new Sequelize('node-complete','root','',{
+    dialect:'mysql',
     host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'9844'
 });
 
-module.exports =pool.promise();
+module.exports=sequelize;
